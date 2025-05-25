0bcfaf472aa43cb2cb885e8a5b063b9a6df20ad3
nico233 <1601550204@qq.com>
2025-05-25 11:48:39 +0800

    添加Mesh

---
+++ includes/Mesh.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <vector>
+#include <memory>
+
+#include "Shader.hpp"
+#include "glad_wrapper.hpp"
+#include "Texture.hpp"
+
+
+struct Vertex {
+  glm::vec3 position;
+  glm::vec3 normal;
+  glm::vec2 texCoords;
+};
+
+class Mesh {
+ public:
+  std::vector<Vertex> vertices{};
+  std::vector<unsigned int> indices{};
+  std::vector<Texture> textures{};
+
+  Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices,
+       std::vector<Texture> textures);
+  void draw(Shader& shader);
+
+ private:
+  std::unique_ptr<glad::VertexArray<Vertex>> vao_{};
+  void setup_mesh();
+};
---
+++ src/Mesh.cpp
@@ -0,0 +1,38 @@
+#include "Mesh.hpp"
+
+
+Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices,
+           std::vector<Texture> textures)
+    : vertices(std::move(vertices)),
+      indices(std::move(indices)),
+      textures(std::move(textures)) {
+  setup_mesh();
+}
+
+void Mesh::setup_mesh() {
+  auto layout = std::make_shared<glad::VertexBufferLayout>(
+      std::vector<glad::VertexAttribute>{
+          {0, "Position", glad::ArrtibuteType::Position},
+          {1, "Normal", glad::ArrtibuteType::Normal},
+          {2, "TexCoords", glad::ArrtibuteType::TexCoords}});
+
+  vao_ = std::make_unique<glad::VertexArray<Vertex>>();
+  vao_->bind();
+  vao_->set_vbo(vertices, layout);
+  vao_->set_ebo(indices);
+
+  vao_->unbind();
+}
+
+void Mesh::draw(Shader& shader) {
+  for (int i = 0; i < textures.size(); i++) {
+    auto& texture = textures[i];
+    texture.bind();
+    shader.set_int(texture.unform_name(), texture.unit_index());
+  }
+  glActiveTexture(GL_TEXTURE0);
+
+  vao_->bind();
+  glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+  vao_->unbind();
+}
